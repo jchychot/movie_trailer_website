ae136bd4ac686e17a45b0a7d9dab413114da8ff2
James Chychota <jchychot@amdocs.com>
2018-04-15 08:48:49 -0400

    Added some comments and removed storyline since its not required in the project

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,46 +1,37 @@
 import media
 import fresh_tomatoes
+
+
+# Movie instances in library
+#Toy Story
 toy_story = media.Movie("Toy Story",
-                        "A story of a boy and his toys that come to life",
                         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=KYz2wyBy3kc" )
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
 avatar = media.Movie("Avatar",
-                     "A marine on an alien planet",
                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://youtu.be/5PSNL1qE6VY")
+
 pulp_fiction = media.Movie("Pulp Fiction",
-                           "Vincent Vega (John Travolta) and Jules Winnfield"
-                           "(Samuel L. Jackson) are hitmen with a penchant for"
-                           "philosophical discussions.",
                            "https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg",
                            "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
+
 ghostbusters = media.Movie("Ghostbusters",
-                           "After the members of a team of scientists"
-                           " (Harold Ramis, Dan Aykroyd, Bill Murray) lose their cushy positions at a university in New"
-                           " York City, they decide to become ghostbusters to wage a high-tech battle with the"
-                           " supernatural for money. They stumble upon a gateway to another dimension, a doorway that"
-                           " will release evil upon the city. The Ghostbusters must now save New York from complete"
-                           " destruction.",
                            "https://upload.wikimedia.org/wikipedia/en/2/2f/Ghostbusters"
                            "_%281984%29_theatrical_poster.png",
                            "https://www.youtube.com/watch?v=BbGkEP9XwRk")
+
 airplane = media.Movie("Airplane!",
-                       "This spoof comedy takes shots at the slew of disaster movies that were released in the 70s."
-                       " When the passengers and crew of a jet are incapacitated due to food poisoning, a rogue pilot"
-                       " with a drinking problem must cooperate with his ex-girlfriend turned stewardess to bring the"
-                       "plane to a safe landing.",
                        "https://upload.wikimedia.org/wikipedia/en/f/f5/Airplane%21.jpg",
                        "https://www.youtube.com/watch?v=rzRJWy-3_Dc")
-caddyshack = media.Movie("Caddyshack",
-                          "Danny Noonan (Michael O'Keefe), a teen down on his luck, works as a caddy at the"
-                          " snob-infested Bushwood Country Club to raise money for his college education. In an attempt"
-                          " to gain votes for a college scholarship reserved for caddies, Noonan volunteers to caddy"
-                          " for a prominent and influential club member (Ted Knight). Meanwhile, Danny struggles to"
-                          " prepare for the high pressure Caddy Day golf tournament while absorbing New Age advice from"
-                          " wealthy golf guru Ty Webb (Chevy Chase).",
-                          "https://upload.wikimedia.org/wikipedia/en/8/84/Caddyshack_poster.jpg",
-                          "https://www.youtube.com/watch?v=x9Nl39uWEYk")
 
+caddyshack = media.Movie("Caddyshack",
+                        "https://upload.wikimedia.org/wikipedia/en/8/84/Caddyshack_poster.jpg",
+                        "https://www.youtube.com/watch?v=x9Nl39uWEYk")
+
+# Group all the movies instances together in a sorted list
 movies = [toy_story, avatar, pulp_fiction, ghostbusters, airplane, caddyshack]
 movies.sort(key=lambda x: x.title,)
+
+# Generate a website that displays these movies
 fresh_tomatoes.open_movies_page(movies)
