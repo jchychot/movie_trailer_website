9658abf54d1ec03f9efcdc7cb888540ecf61c122
James Chychota <jchychot@amdocs.com>
2018-04-15 09:32:37 -0400

    Added some comments and format for PEP8

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,31 +3,36 @@ import fresh_tomatoes
 
 
 # Movie instances in library
-#Toy Story
 toy_story = media.Movie("Toy Story",
-                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://upload.wikimedia.org/wikipedia/en/1/13/"
+                        "Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 avatar = media.Movie("Avatar",
-                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/"
+                     "Avatar-Teaser-Poster.jpg",
                      "https://youtu.be/5PSNL1qE6VY")
 
 pulp_fiction = media.Movie("Pulp Fiction",
-                           "https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg",
+                           "https://upload.wikimedia.org/wikipedia/en/3/3b/"
+                           "Pulp_Fiction_%281994%29_poster.jpg",
                            "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
 
 ghostbusters = media.Movie("Ghostbusters",
-                           "https://upload.wikimedia.org/wikipedia/en/2/2f/Ghostbusters"
+                           "https://upload.wikimedia.org/wikipedia/en/2/2f/"
+                           "Ghostbusters"
                            "_%281984%29_theatrical_poster.png",
                            "https://www.youtube.com/watch?v=BbGkEP9XwRk")
 
 airplane = media.Movie("Airplane!",
-                       "https://upload.wikimedia.org/wikipedia/en/f/f5/Airplane%21.jpg",
+                       "https://upload.wikimedia.org/wikipedia/en/f/f5/"
+                       "Airplane%21.jpg",
                        "https://www.youtube.com/watch?v=rzRJWy-3_Dc")
 
 caddyshack = media.Movie("Caddyshack",
-                        "https://upload.wikimedia.org/wikipedia/en/8/84/Caddyshack_poster.jpg",
-                        "https://www.youtube.com/watch?v=x9Nl39uWEYk")
+                         "https://upload.wikimedia.org/wikipedia/en/8/84/"
+                         "Caddyshack_poster.jpg",
+                         "https://www.youtube.com/watch?v=x9Nl39uWEYk")
 
 # Group all the movies instances together in a sorted list
 movies = [toy_story, avatar, pulp_fiction, ghostbusters, airplane, caddyshack]
--- media.py
+++ media.py
@@ -1,13 +1,13 @@
-import webbrowser
 
 class Movie():
-    """ This class provides a way to store movie related information """
-    VALID_RATINGS= ["G", "PG", "PG-13", "R"]
+    """ This class provides a way to store movie related information.
 
+    Included movie title, poster image and the movies trailer from
+    youtube.
+    """
+
+    # Movie class data structure
     def __init__(self, movie_title, poster_image, trailer_youtube):
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
